93215abdbfa587f823c2780588a610ae6952d82d
Liquan (Max) Gu <maxlg@chromium.org>
2021-12-08 12:22:38 -0500

    compilable

--- com.bobpay/src/main/java/com/bobpay/PaymentActivity.java
+++ com.bobpay/src/main/java/com/bobpay/PaymentActivity.java
@@ -2,7 +2,7 @@ package com.bobpay;
 
 import android.content.Intent;
 import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
